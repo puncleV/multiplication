a33777ca59a6d67f4f4ae1f4f9b5c43166ac1534
punkkk <punkrus18@gmail.com>
2020-01-21 15:30:37 +0300

    add: sum method

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
+const _ = require('lodash');
+
 const f = '3141592653589793238462643383279502884197169399375105820974944592';
 const s = '2718281828459045235360287471352662497757247093699959574966967627';
 
-const multiply = (a, b) => {
-  return a * b;
+const sum = (a, b) => {
+  const result = [];
+
+  let co = 0;
+  let longest = a.length > b.length ? a.length : b.length;
+  for (let i = longest - 1; i >= 0; i--) {
+      const sum = parseInt(a[i] || 0, 10) + parseInt(b[i] || 0, 10) + co;
+
+      if (sum >= 10) {
+        result.push(sum - 10);
+        co = 1;
+      } else {
+        result.push(sum)
+        co = 0;
+      }
+  }
+
+  if (co !== 0) {
+    result.push(co)
+  }
+
+  return result.reverse().join('');
 }
 
-console.log(multiply(f, s))
+const multiply = (f, s) => {
+  for (let i = s.length - 1; i >= 0; i--) {
+    for (let j = f.length - 1; j >= 0; j--) {
+
+    }
+  }
+
+  return (f * s).toString();
+}
+
